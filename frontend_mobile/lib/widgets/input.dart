2f7c3cf88034c6020e4d9d532299029bb7f50ca9
noormesslmani <noor.messlmani@gmail.com>
2022-12-15 02:02:04 +0200

    Frontend: creating an input widget

---
+++ frontend_mobile/lib/widgets/input.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Input extends StatefulWidget {
+  final FocusNode focusNode;
+  final Icon prefixIcon;
+  final IconButton? suffixIcon;
+  final String label;
+  final bool obscureText;
+  const Input(
+      {required this.prefixIcon,
+      required this.label,
+      required this.focusNode,
+      required this.suffixIcon,
+      required this.obscureText,
+      super.key});
+
+  @override
+  State<Input> createState() => _InputState();
+}
+
+class _InputState extends State<Input> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      focusNode: widget.focusNode,
+      onTap: () => {
+        setState(() {
+          FocusScope.of(context).requestFocus(widget.focusNode);
+        }),
+      },
+      obscureText: widget.obscureText,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(
+            color: Theme.of(context).focusColor,
+            width: 1,
+          ),
+        ),
+        labelText: widget.label,
+        labelStyle: TextStyle(
+          color: widget.focusNode.hasFocus
+              ? Theme.of(context).primaryColorDark
+              : Colors.grey,
+        ),
+        prefixIcon: widget.prefixIcon,
+        suffixIcon: widget.suffixIcon,
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(
+            color: Theme.of(context).errorColor,
+            width: 1,
+          ),
+        ),
+      ),
+    );
+  }
+}
