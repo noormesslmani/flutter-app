6f086d72f25a15508ab9c003278af867315f0021
noormesslmani <noor.messlmani@gmail.com>
2022-12-16 01:30:22 +0200

    Frontend: using typeCard widget and setting _usertype state onTap

--- frontend_mobile/lib/screens/auth/Sign_up3.dart
+++ frontend_mobile/lib/screens/auth/Sign_up3.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:frontend_mobile/widgets/buttons/auth_button.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'package:frontend_mobile/widgets/reusable_widgets.dart';
+import 'package:frontend_mobile/widgets/cards/user_type.dart';
 
 class SignUp3 extends StatefulWidget {
   const SignUp3({super.key});
@@ -14,6 +15,8 @@ class SignUp3 extends StatefulWidget {
 }
 
 class _SignUp3State extends State<SignUp3> {
+  String _userType = 'owner';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,57 +44,36 @@ class _SignUp3State extends State<SignUp3> {
                 const SizedBox(
                   height: 15,
                 ),
-                GestureDetector(
+                TypeCard(
+                  type: 'Pet Owner',
+                  color: const Color.fromRGBO(200, 239, 232, 1),
+                  image: const AssetImage("assets/images/owner.png"),
                   onTap: () {
-                    debugPrint("Tapped a Container");
+                    setState(() {
+                      _userType = 'owner';
+                    });
                   },
-                  child: Container(
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Theme.of(context).primaryColorLight,
-                    ),
-                    height: MediaQuery.of(context).size.height * 0.15,
-                    width: double.infinity,
-                    child: const Text(
-                      'Pet Owner',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    debugPrint("Tapped a Container");
-                  },
-                  child: Container(
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Theme.of(context).primaryColorLight,
-                    ),
-                    height: MediaQuery.of(context).size.height * 0.15,
-                    width: double.infinity,
-                    child: const Text(
-                      'Service Provider',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
-                      ),
-                    ),
-                  ),
+                  opacity: _userType == 'owner' ? 1 : 0.4,
                 ),
                 const SizedBox(
                   height: 10,
                 ),
-                AuthBottun(
+                TypeCard(
+                  type: 'Service provider',
+                  color: const Color.fromRGBO(240, 199, 200, 1),
+                  image: const AssetImage("assets/images/service.png"),
+                  onTap: () {
+                    setState(() {
+                      _userType = 'provider';
+                    });
+                    debugPrint(_userType);
+                  },
+                  opacity: _userType == 'provider' ? 1 : 0.4,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const AuthButton(
                   label: 'Create Account',
                   width: double.infinity,
                   handlePress: null,
@@ -99,13 +81,13 @@ class _SignUp3State extends State<SignUp3> {
               ],
             ),
           ),
-          const Spacer(),
-          ClipPath(
-            clipper: WaveClipperOne(reverse: true),
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.15,
-              width: MediaQuery.of(context).size.width,
-              color: Theme.of(context).primaryColorLight,
+          Expanded(
+            child: ClipPath(
+              clipper: WaveClipperOne(reverse: true),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                color: Theme.of(context).primaryColorLight,
+              ),
             ),
           ),
         ],
