bd1e39bf7ab8237d54d778dac456b6db9b748416
noormesslmani <noor.messlmani@gmail.com>
2022-12-16 00:53:53 +0200

    Frontend: replacing stateful with stateless widget

--- frontend_mobile/lib/widgets/buttons/auth_button.dart
+++ frontend_mobile/lib/widgets/buttons/auth_button.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
 
-class AuthBottun extends StatefulWidget {
+class AuthButton extends StatelessWidget {
   final String label;
   final double width;
   final void Function()? handlePress;
-  const AuthBottun({
+  const AuthButton({
     required this.label,
     required this.width,
     required this.handlePress,
     super.key,
   });
 
-  @override
-  State<AuthBottun> createState() => _AuthBottunState();
-}
-
-class _AuthBottunState extends State<AuthBottun> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -24,16 +19,16 @@ class _AuthBottunState extends State<AuthBottun> {
         borderRadius: BorderRadius.circular(20),
       ),
       child: ElevatedButton(
-        onPressed: widget.handlePress,
+        onPressed: handlePress,
         style: ElevatedButton.styleFrom(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
           ),
-          minimumSize: Size(widget.width, 50),
+          minimumSize: Size(width, 50),
           backgroundColor: Theme.of(context).primaryColor,
         ),
         child: Text(
-          widget.label,
+          label,
           style: const TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.bold,
