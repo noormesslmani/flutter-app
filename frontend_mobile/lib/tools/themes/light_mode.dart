e963492b604853178dc2aaa225e7b671890f6320
noormesslmani <noor.messlmani@gmail.com>
2022-12-15 02:00:34 +0200

    Frontend: adding some items to light mode theme

--- frontend_mobile/lib/tools/themes/light_mode.dart
+++ frontend_mobile/lib/tools/themes/light_mode.dart
@@ -2,14 +2,24 @@ import 'package:flutter/material.dart';
 
 class LightTheme {
   ThemeData theme = ThemeData(
-      primaryColor: const Color.fromRGBO(122, 188, 178, 1),
-      primaryColorLight: const Color.fromRGBO(200, 239, 232, 1),
-      primaryColorDark: const Color.fromRGBO(33, 76, 69, 1),
-      backgroundColor: Colors.white,
-      colorScheme: ColorScheme.fromSwatch().copyWith(
-        secondary: const Color.fromRGBO(242, 158, 146, 1),
-        tertiary: Colors.grey[400],
-      ));
+    primaryColor: const Color.fromRGBO(122, 188, 178, 1),
+    primaryColorLight: const Color.fromRGBO(200, 239, 232, 1),
+    primaryColorDark: const Color.fromRGBO(33, 76, 69, 1),
+    backgroundColor: Colors.white,
+    focusColor: const Color.fromRGBO(122, 188, 178, 1),
+    errorColor: const Color.fromARGB(255, 195, 29, 26),
+    colorScheme: ColorScheme.fromSwatch().copyWith(
+      secondary: const Color.fromRGBO(242, 158, 146, 1),
+      tertiary: Colors.grey[400],
+    ),
+    fontFamily: 'Inter',
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(
+          fontSize: 30,
+          fontFamily: 'FredokaOne',
+          color: Color.fromRGBO(122, 188, 178, 1)),
+    ),
+  );
 
   ThemeData get themeData => theme;
 }
