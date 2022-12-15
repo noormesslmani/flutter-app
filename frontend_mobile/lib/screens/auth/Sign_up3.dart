24f715475f0b859423da4e96253ca11a26a24a36
noormesslmani <noor.messlmani@gmail.com>
2022-12-15 18:32:43 +0200

    Frontend: creating SignUp3 screen

---
+++ frontend_mobile/lib/screens/auth/Sign_up3.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend_mobile/widgets/buttons/auth_button.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+
+class SignUp3 extends StatefulWidget {
+  const SignUp3({super.key});
+
+  static const routeName = '/login';
+
+  @override
+  State<SignUp3> createState() => _SignUp3State();
+}
+
+class _SignUp3State extends State<SignUp3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: const Image(
+              image: AssetImage("assets/logo/logo.png"),
+              height: 120,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'You are...?',
+                  style: TextStyle(
+                    fontSize: 26,
+                    color: Colors.grey[800],
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    debugPrint("Tapped a Container");
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Theme.of(context).primaryColorLight,
+                    ),
+                    height: MediaQuery.of(context).size.height * 0.15,
+                    width: double.infinity,
+                    child: const Text(
+                      'Pet Owner',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    debugPrint("Tapped a Container");
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Theme.of(context).primaryColorLight,
+                    ),
+                    height: MediaQuery.of(context).size.height * 0.15,
+                    width: double.infinity,
+                    child: const Text(
+                      'Service Provider',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                AuthBottun(
+                  label: 'Create Account',
+                  width: double.infinity,
+                  handlePress: null,
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          ClipPath(
+            clipper: WaveClipperOne(reverse: true),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.15,
+              width: MediaQuery.of(context).size.width,
+              color: Theme.of(context).primaryColorLight,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
