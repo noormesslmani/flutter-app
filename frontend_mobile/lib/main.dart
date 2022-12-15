7bf6eba1babf2126eb7f31d6dcb133262189ae84
noormesslmani <noor.messlmani@gmail.com>
2022-12-15 18:05:40 +0200

    Frontend: adding SignUp2 to routes

--- frontend_mobile/lib/main.dart
+++ frontend_mobile/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:frontend_mobile/screens/onboarding.dart';
 import 'package:frontend_mobile/tools/themes/light_mode.dart';
 import 'package:frontend_mobile/screens/auth/Sign_in.dart';
 import 'package:frontend_mobile/screens/auth/Sign_up1.dart';
+import 'package:frontend_mobile/screens/auth/Sign_up2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
         "/onboarding": (context) => const Onboarding(),
         "/signin": (context) => SignIn(),
         "/signup1": (context) => SignUp1(),
+        "/signup2": (context) => SignUp2(),
       },
     );
   }
