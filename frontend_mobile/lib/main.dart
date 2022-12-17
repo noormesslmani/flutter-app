40553a427dbdc9bf1f5134e1a99104f8ffab5ebf
noormesslmani <noor.messlmani@gmail.com>
2022-12-18 00:25:16 +0200

    Frontend: wrapping the app in main file with providers

--- frontend_mobile/lib/main.dart
+++ frontend_mobile/lib/main.dart
@@ -6,6 +6,9 @@ import 'package:frontend_mobile/screens/auth/Sign_up1.dart';
 import 'package:frontend_mobile/screens/auth/Sign_up2.dart';
 import 'package:frontend_mobile/screens/auth/Sign_up3.dart';
 import 'package:frontend_mobile/screens/tabs.dart';
+import 'package:provider/provider.dart';
+import './providers/auth.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,18 +18,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: LightTheme().themeData,
-      debugShowCheckedModeBanner: false,
-      home: const Onboarding(),
-      routes: {
-        "/onboarding": (context) => const Onboarding(),
-        "/signin": (context) => SignIn(),
-        "/signup1": (context) => SignUp1(),
-        "/signup2": (context) => SignUp2(),
-        "/signup3": (context) => SignUp3(),
-        "/tabs": (context) => Tabs(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<Auth>(
+          create: (_) => Auth(),
+        ),
+      ],
+      child: Consumer<Auth>(
+        builder: (context, auth, _) => MaterialApp(
+          theme: LightTheme().themeData,
+          debugShowCheckedModeBanner: false,
+          home: const Onboarding(),
+          routes: {
+            "/onboarding": (context) => const Onboarding(),
+            "/signin": (context) => SignIn(),
+            "/signup1": (context) => SignUp1(),
+            "/signup2": (context) => SignUp2(),
+            "/signup3": (context) => SignUp3(),
+            "/tabs": (context) => Tabs(),
+          },
+        ),
+      ),
     );
   }
 }
