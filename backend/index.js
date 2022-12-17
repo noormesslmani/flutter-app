e9111e72372d1647742bbb0f7fc38a4dff7097b6
noormesslmani <noor.messlmani@gmail.com>
2022-12-17 13:37:28 +0200

    Backend: requiring and using cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 require('dotenv').config();
 require('./config/db.config')
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+app.use(cors());
 
 
-//prot is in env
+const authRoutes = require('./routes/auth.routes');
+app.use('/auth', authRoutes) //path extension for auth
+
+//port is in env
 app.listen(process.env.PORT, (err)=>{
     if(err) throw err;
     console.log(`server running on port ${process.env.PORT}`);
