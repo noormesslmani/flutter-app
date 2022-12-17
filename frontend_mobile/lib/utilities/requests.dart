cc82be8f56328847c978f010a38b703f3f4d73be
noormesslmani <noor.messlmani@gmail.com>
2022-12-18 00:44:03 +0200

    Frontend: adding Requests and HttpException classes to utilities

---
+++ frontend_mobile/lib/utilities/requests.dart
@@ -0,0 +1,19 @@
+import 'package:frontend_mobile/providers/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Requests {
+  static String url = dotenv.env['url'].toString();
+  Map<String, String> headers = {"Content-Type": "application/json"};
+
+  Map<String, String> getHeaders(context) {
+    final token = Provider.of<Auth>(context, listen: false).getToken;
+    if (token != null) {
+      return {
+        ...headers,
+        "Authorization": "Bearer $token",
+      };
+    }
+    return headers;
+  }
+}
