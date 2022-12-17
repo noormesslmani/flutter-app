b44d46e93511848ca30dc20fb2c4d2ec7a1604cd
noormesslmani <noor.messlmani@gmail.com>
2022-12-18 00:45:26 +0200

    Frontend: creating AuthService class and adding a logIn method

---
+++ frontend_mobile/lib/services/auth_service.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../utilities/requests.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:frontend_mobile/providers/auth.dart';
+
+class AuthService with ChangeNotifier {
+  Future<void> login(String email, String password, context) async {
+    try {
+      final url = Uri.http(Requests.url, '/auth/login');
+      final response = await http.post(url,
+          body: json.encode({'email': email, 'password': password}),
+          headers: Requests().getHeaders(context));
+      final data = json.decode(response.body);
+
+      if (response.statusCode != 200) {
+        throw HttpException(data["message"]);
+      }
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString("token", data["token"]);
+
+      Auth().setToken(data["token"]);
+      Auth().setUser(data['user']);
+      notifyListeners();
+      debugPrint(data["user"]["email"]);
+    } catch (error) {
+      rethrow;
+    }
+  }
+}
