5d52478bf601f792c0d0d89ab05f3c69f723092e
noormesslmani <noor.messlmani@gmail.com>
2022-12-23 01:06:52 +0200

    Frontend: creating ServiceCardUtilities class

---
+++ frontend_mobile/lib/utilities/serviceCard.dart
@@ -0,0 +1,5 @@
+class ServiceCardUtilities {
+  static String path = 'assets/images/';
+  static List<String> images = <String>['${path}medical.png', '${path}pet-theraphy.png', '${path}dog.png','${path}walk.png','${path}bath.png','${path}pet-boarding.png'];
+  static List<String> titles = <String>['Veterinary', 'Sitting', 'Walking','Training','Grooming','Boarding'];
+}
