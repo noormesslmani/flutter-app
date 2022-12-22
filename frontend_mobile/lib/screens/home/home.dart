79d1c7225b827f46ad7ed91b4f2234406b3f9d57
noormesslmani <noor.messlmani@gmail.com>
2022-12-22 23:20:50 +0200

    Frontend: creating a search bar in home screen

--- frontend_mobile/lib/screens/home/home.dart
+++ frontend_mobile/lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_mobile/widgets/input.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,10 +9,40 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String searchQuery = '';
+  FocusNode searchFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home'),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Input(
+            setData: (text) {
+              setState(
+                () {
+                  searchQuery = text;
+                },
+              );
+              debugPrint(searchQuery);
+            },
+            prefixIcon: const Icon(
+              Icons.search,
+              color: Colors.grey,
+            ),
+            label: 'Search',
+            focusNode: searchFocusNode,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Services",
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+        ],
+      ),
     );
   }
 }
