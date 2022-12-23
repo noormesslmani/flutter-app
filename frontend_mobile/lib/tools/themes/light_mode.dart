f49db5f061bfd2f9f14b4a17fd04f06bea5dd53f
noormesslmani <noor.messlmani@gmail.com>
2022-12-23 17:52:29 +0200

    Frontend: changing some text style themes

--- frontend_mobile/lib/tools/themes/light_mode.dart
+++ frontend_mobile/lib/tools/themes/light_mode.dart
@@ -5,9 +5,10 @@ class LightTheme {
     primaryColor: const Color.fromRGBO(122, 188, 178, 1),
     primaryColorLight: const Color.fromRGBO(200, 239, 232, 1),
     primaryColorDark: const Color.fromRGBO(33, 76, 69, 1),
-    backgroundColor: Colors.white,
+    backgroundColor: const Color.fromRGBO(246, 246, 246, 1),
     focusColor: const Color.fromRGBO(122, 188, 178, 1),
     errorColor: const Color.fromARGB(255, 195, 29, 26),
+    indicatorColor: const Color.fromRGBO(242, 158, 146, 1),
     colorScheme: ColorScheme.fromSwatch().copyWith(
       secondaryContainer: const Color.fromRGBO(242, 158, 146, 1),
       secondary: const Color.fromRGBO(240, 199, 200, 1),
@@ -28,10 +29,26 @@ class LightTheme {
     fontFamily: 'Inter',
     textTheme: const TextTheme(
       titleLarge: TextStyle(
-        fontSize: 22,
-        fontFamily: 'Inter',
         fontWeight: FontWeight.bold,
       ),
+      titleMedium: TextStyle(
+        fontWeight: FontWeight.bold,
+      ),
+      titleSmall: TextStyle(
+        fontWeight: FontWeight.bold,
+      ),
+      labelLarge: TextStyle(
+        fontWeight: FontWeight.w600,
+      ),
+      labelMedium: TextStyle(
+        fontWeight: FontWeight.w700,
+      ),
+      labelSmall: TextStyle(
+        fontWeight: FontWeight.w700,
+      ),
+    ).apply(
+      bodyColor: Color.fromRGBO(64, 64, 64, 1),
+      displayColor: Color.fromRGBO(64, 64, 64, 1),
     ),
   );
 
