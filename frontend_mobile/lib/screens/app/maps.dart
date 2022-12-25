9ac0b72f2cec4aa9fd34794595b12c3f4d4b777e
noormesslmani <noor.messlmani@gmail.com>
2022-12-25 20:55:32 +0200

    Frontend: displaying current location on google map

--- frontend_mobile/lib/screens/app/maps.dart
+++ frontend_mobile/lib/screens/app/maps.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 import 'package:frontend_mobile/widgets/app_bar.dart';
+import 'package:frontend_mobile/services/location_service.dart';
 
 class Maps extends StatefulWidget {
   const Maps({Key? key}) : super(key: key);
@@ -14,16 +15,21 @@ class MapsState extends State<Maps> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
+  static const CameraPosition _Beirut = CameraPosition(
     target: LatLng(33.888630, 35.495480),
     zoom: 12,
   );
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 0,
-      target: LatLng(33.888630, 35.495480),
-      tilt: 59.440717697143555,
-      zoom: 15);
+  final List<Marker> _markers = <Marker>[
+    const Marker(
+      markerId: MarkerId('1'),
+      icon: BitmapDescriptor.defaultMarker,
+      position: LatLng(33.888630, 35.495480),
+      infoWindow: InfoWindow(
+        title: 'Beirut',
+      ),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -35,21 +41,38 @@ class MapsState extends State<Maps> {
       ),
       body: GoogleMap(
         mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
+        markers: Set<Marker>.of(_markers),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        compassEnabled: true,
+        initialCameraPosition: _Beirut,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
+        onPressed: () async {
+          LocationService.determinePosition().then((value) async {
+            _gotToCurrentLocation(value);
+          });
+        },
+        label: const Text('To current location!'),
         icon: const Icon(Icons.directions_boat),
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _gotToCurrentLocation(value) async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          bearing: 0,
+          target: LatLng(value.latitude, value.longitude),
+          tilt: 59.440717697143555,
+          zoom: 15,
+        ),
+      ),
+    );
   }
 }
