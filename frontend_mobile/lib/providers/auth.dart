737ebef910e0ce544b3a8797ba69d02ff198df26
noormesslmani <noor.messlmani@gmail.com>
2022-12-25 22:46:37 +0200

    Frontend: changing type of _user  to CurrentUser

--- frontend_mobile/lib/providers/auth.dart
+++ frontend_mobile/lib/providers/auth.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_mobile/models/user.dart';
 
 class Auth with ChangeNotifier {
-  Map<String, dynamic>? _user;
+  CurrentUser _user = CurrentUser(
+    id: '',
+    name: '',
+    country: '',
+    dob: '',
+    email: '',
+    phone: 0,
+    userType: '',
+    profilePicture: '',
+  );
+  // Map<String, dynamic>? _user;
   String? _token;
 
   String? get getToken {
@@ -12,11 +23,14 @@ class Auth with ChangeNotifier {
     return null;
   }
 
-  Map<String, dynamic>? get getUser {
+  // Map<String, dynamic>? get getUser {
+  //   return _user;
+  // }
+  CurrentUser? get getUser {
     return _user;
   }
 
-  void setUser(Map<String, dynamic>? user) {
+  void setUser(CurrentUser user) {
     _user = user;
     notifyListeners();
   }
