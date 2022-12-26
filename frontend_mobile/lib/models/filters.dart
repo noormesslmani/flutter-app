5f824b6ec838e26434b52d6f805b43a12765ec16
noormesslmani <noor.messlmani@gmail.com>
2022-12-26 20:39:35 +0200

    Frontend: creating Filters model

---
+++ frontend_mobile/lib/models/filters.dart
@@ -0,0 +1,11 @@
+class Filters {
+  String sortBy;
+  double distance;
+  int minRating;
+
+  Filters({
+    required this.distance,
+    required this.sortBy,
+    required this.minRating,
+  });
+}
