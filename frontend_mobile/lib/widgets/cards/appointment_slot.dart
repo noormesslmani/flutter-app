9213531e5e03c5ee793e3586b0b26994866614b2
noormesslmani <noor.messlmani@gmail.com>
2022-12-29 16:25:20 +0200

    Frontend: creating AppointmentSlot widget

---
+++ frontend_mobile/lib/widgets/cards/appointment_slot.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppointmentSlot extends StatelessWidget {
+  final String time;
+  final int? selectedIndex;
+  final int index;
+  const AppointmentSlot(
+      {required this.time,
+      required this.selectedIndex,
+      required this.index,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: selectedIndex == index
+              ? Theme.of(context).primaryColor
+              : Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Theme.of(context).primaryColor,
+          )),
+      child: Text(
+        time,
+        style: Theme.of(context)
+            .textTheme
+            .labelSmall!
+            .apply(color: selectedIndex == index ? Colors.white : null),
+      ),
+    );
+  }
+}
