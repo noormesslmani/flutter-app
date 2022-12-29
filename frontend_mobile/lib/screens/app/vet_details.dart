41516a576e5769408b1c85b326e4d8a6b5fd8955
noormesslmani <noor.messlmani@gmail.com>
2022-12-29 18:40:18 +0200

    Frontend: finishing VetDetails screen UI

--- frontend_mobile/lib/screens/app/vet_details.dart
+++ frontend_mobile/lib/screens/app/vet_details.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:frontend_mobile/widgets/app_bar.dart';
+import 'package:frontend_mobile/widgets/cards/vet_details_card.dart';
+import 'package:frontend_mobile/widgets/text_expand.dart';
+import 'package:horizontal_calendar/horizontal_calendar.dart';
+import 'package:frontend_mobile/widgets/cards/appointment_slot.dart';
+import 'package:frontend_mobile/widgets/dialogs/confirm_dialog.dart';
 
 class VetDetails extends StatefulWidget {
   const VetDetails({super.key});
@@ -10,6 +14,15 @@ class VetDetails extends StatefulWidget {
 
 class _VetDetailsState extends State<VetDetails> {
   bool _isFavorite = false;
+  bool _isHidden = true;
+  int? _selectedIndex;
+  final List timeSlots = [
+    '8:00-9:00',
+    '9:00-10:00',
+    '10:00-11:00',
+    '11:00-12:00',
+    '12:00-13:00'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,6 +85,206 @@ class _VetDetailsState extends State<VetDetails> {
                 color: Theme.of(context).backgroundColor,
                 borderRadius: const BorderRadius.vertical(
                   top: Radius.circular(30),
+                ),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      child: Stack(
+                        alignment: AlignmentDirectional.center,
+                        children: [
+                          Align(
+                            child: Text(
+                              'Dr. Sarah Smith',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge!
+                                  .apply(
+                                    color: Theme.of(context).primaryColorDark,
+                                  ),
+                            ),
+                          ),
+                          Positioned(
+                            right: 5,
+                            child: SizedBox(
+                              width: 40,
+                              height: 40,
+                              child: FloatingActionButton(
+                                onPressed: () {},
+                                child: const Icon(
+                                  Icons.chat_bubble,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.work,
+                          color: Colors.grey,
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          '4 years',
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 14,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        VetDetailCard(
+                          icon: Icon(
+                            Icons.star,
+                            color: Colors.yellow[700],
+                            size: 30,
+                          ),
+                          text: '4.5/5',
+                          title: 'Rating',
+                        ),
+                        VetDetailCard(
+                          icon: Icon(
+                            Icons.location_pin,
+                            color: Theme.of(context).colorScheme.secondary,
+                            size: 30,
+                          ),
+                          text: '3 Km',
+                          title: 'Location',
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Expanded(
+                      child: ListView(
+                        children: [
+                          Text(
+                            'About',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .apply(
+                                    color: Theme.of(context).primaryColorDark),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+                            maxLines: _isHidden ? 2 : 10,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: TextExpand(
+                              isHidden: _isHidden,
+                              color: Theme.of(context).primaryColor,
+                              onTap: () {
+                                setState(
+                                  () {
+                                    _isHidden = !_isHidden;
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Appointments',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .apply(
+                                    color: Theme.of(context).primaryColorDark),
+                          ),
+                          HorizontalCalendar(
+                            date: DateTime.now().add(const Duration(days: 3)),
+                            lastDate:
+                                DateTime.now().add(const Duration(days: 60)),
+                            textColor: Colors.black45,
+                            backgroundColor: Theme.of(context).backgroundColor,
+                            selectedColor: Theme.of(context).primaryColor,
+                            onDateSelected: (date) => print(
+                              date.toString(),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Available Slots',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleSmall!
+                                .apply(
+                                    color: Theme.of(context).primaryColorDark),
+                          ),
+                          SizedBox(
+                            height: 60,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 10),
+                              child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: timeSlots.length,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    return GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _selectedIndex = index;
+                                        });
+                                      },
+                                      onDoubleTap: () {
+                                        showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return StatefulBuilder(
+                                              builder: (BuildContext context,
+                                                  StateSetter setState) {
+                                                return const ConfirmDialig(
+                                                    text:
+                                                        'Are you sure you want to book the appointment?');
+                                              },
+                                            );
+                                          },
+                                        );
+                                      },
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 5),
+                                        child: AppointmentSlot(
+                                          time: timeSlots[index],
+                                          index: index,
+                                          selectedIndex: _selectedIndex,
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
