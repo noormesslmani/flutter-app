a6ab471f9cf5c714abe82afa6038dcae9409c1df
noormesslmani <noor.messlmani@gmail.com>
2022-12-30 21:14:58 +0200

    Frontend; adding an arrow IconButton to ServiceProviderCard

--- frontend_mobile/lib/widgets/cards/service_provider_card.dart
+++ frontend_mobile/lib/widgets/cards/service_provider_card.dart
@@ -40,48 +40,68 @@ class ServiceProviderCard extends StatelessWidget {
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(7),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          'Sarah Smith',
-                          style: Theme.of(context).textTheme.labelLarge,
-                        ),
-                        Row(
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Row(
-                              children: [
-                                Text(
-                                  '4',
-                                  style:
-                                      Theme.of(context).textTheme.labelMedium,
-                                ),
-                                Icon(
-                                  Icons.star,
-                                  color: Colors.yellowAccent[700],
-                                  size: 20,
-                                ),
-                              ],
+                            Text(
+                              'Sarah Smith',
+                              style: Theme.of(context).textTheme.labelLarge,
                             ),
                             Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                Icon(
-                                  Icons.location_pin,
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .secondaryContainer,
-                                  size: 20,
+                                Row(
+                                  children: [
+                                    Text(
+                                      '4',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .labelMedium,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.yellowAccent[700],
+                                      size: 20,
+                                    ),
+                                  ],
                                 ),
-                                Text(
-                                  '5Km',
-                                  style:
-                                      Theme.of(context).textTheme.labelMedium,
-                                )
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.location_pin,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondaryContainer,
+                                      size: 20,
+                                    ),
+                                    Text(
+                                      '5Km',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .labelMedium,
+                                    )
+                                  ],
+                                ),
                               ],
-                            ),
+                            )
                           ],
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.arrow_forward_ios,
+                            color: Theme.of(context).primaryColorDark,
+                            size: 24,
+                          ),
+                          padding: const EdgeInsets.all(0),
+                          alignment: Alignment.centerRight,
                         )
                       ],
                     ),
